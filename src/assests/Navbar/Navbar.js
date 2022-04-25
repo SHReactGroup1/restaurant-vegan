3c6bbd4f26004792b9f576bfc6d18cea5c6954f9
kelue <austincmon5@gmail.com>
2022-04-25 05:31:24 +0100

    fixed button animation

--- src/assests/Navbar/Navbar.js
+++ src/assests/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import Logovegan from '../img/Logo-Vegan.png'
 function Navbar() {
   return (
     <nav className="flex justify-around items-center pa3 ph5-ns">
-        <a className="v-mid link dim mr6" href="/" title="Home">
+        <a className="v-mid link dim" href="/" title="Home">
             <img src={Logovegan} className="w4 h3" alt="Site Name"/>
         </a>
         <div className="v-mid">
@@ -13,8 +13,8 @@ function Navbar() {
             <a className="link dim green f4 f3-ns dib" href="/product" title="Product">Product</a>
         </div>
         <div>
-            <a class="f3 mr4-ns link dim bw1 shape ph4 pv3 mb2 dib white bg-dark-green" href="#0">Sign Up</a>
-            <a class="f3 link dim shape bw1 ba ph4 pv3 mb2 dib dark-green" href="#0">Login</a>
+            <a class="f3 mr4-ns no-underline bw1 grow shape ph4 pv3 mb2 dib white bg-dark-green" href="#0">Sign Up</a>
+            <a class="f3 no-underline shape bw1 ba ph4 pv3 mb2 dib dark-green grow" href="#0">Login</a>
         </div>
     </nav>
   )
